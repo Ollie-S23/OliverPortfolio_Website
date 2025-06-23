b961b21ba022b31def15527f641644a5f00ac416
Oliver Scott <105912692@student.swin.edu.au>
2025-06-23 16:38:36 +1000

    update

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> <!-- Adds web browser support meta tags for format and search algorithm-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="The Optional Group">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="shortcut icon" href="images/OS_icon.png">
+    <title>The Optional Group</title>
 </head>
 <header>
+    <?php include('header.inc')?>
 </header>
 <body>
     
+    <footer>
+        <?php include('footer.inc')?>
+    </footer>
 </body>
 </html>
