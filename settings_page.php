e6a8c3c1fc628a2bda7a99196e6486af0c2fb834
Oliver Scott <105912692@student.swin.edu.au>
2025-06-24 13:09:06 +1000

    add profile icon but the alignment is off

--- settings_page.php
+++ settings_page.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <script src="darkmode.js" defer></script>
 </head>
+<?php
+session_start();
+?>
 <body>
     <h1> Test </h1>
 </body>
