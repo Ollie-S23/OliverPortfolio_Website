e0d57ce4bd0bbc838718943f2830f6cc1d5a5f9b
Oliver Scott <105912692@student.swin.edu.au>
2025-06-26 14:41:47 +1000

    added amanage_posts.php file to manage posts

---
+++ Manage_posts.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> <!-- Adds web browser support meta tags for format and search algorithm-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="The Optional Group">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="shortcut icon" href="styles/images/OS_icon.png">
+    <title>The Optional Group</title>
+    <script type="text/javascript" src="darkmode.js" defer></script>
+</head>
+<?php session_start(); ?>
+<body>
+    <div class="page-wrapper">
+        <header>
+        <?php include('header.inc')?>
+        </header>
+        <main>
+            <p class="manage_posts-p"> Welcome to the Manage Posts page. Here you can view and manage your posts.</p>
+        </main>
+        <footer>
+            <?php include('footer.inc')?>
+        </footer>
+    </div>
+</body>
+</html>
